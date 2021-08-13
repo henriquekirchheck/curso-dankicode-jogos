71babe0c61a11373dfcf568ee121e70ddc5a4157
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-13 10:54:54 -0300

    Learned if / else

--- teste_curso_games/src/teste_curso_games/Game.java
+++ teste_curso_games/src/teste_curso_games/Game.java
@@ -7,5 +7,15 @@ public class Game {
 		double peso = 88.2;
 		boolean tipo = true;
 		System.out.println("Seu nome é " + nome + " e sua idade é " + idade + " e seu peso é " + peso);
+		
+		if (idade == 28) {
+			System.out.println("A idade é 28");
+		} else {
+			System.out.println("Idade correta é " + idade);
+		}
+		
+		if (nome.equals("Henrique")) {
+			System.out.println("O nome é Henrique");
+		}
 	}
 }

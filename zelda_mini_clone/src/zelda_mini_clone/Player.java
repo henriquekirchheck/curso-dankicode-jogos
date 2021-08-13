1c5d3785194a03ccdaa19f9d50efff184de07ac8
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-13 17:22:01 -0300

    BROKEN: Tried to add a sprite to the game

--- zelda_mini_clone/src/zelda_mini_clone/Player.java
+++ zelda_mini_clone/src/zelda_mini_clone/Player.java
@@ -1,6 +1,5 @@
 package zelda_mini_clone;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
@@ -29,7 +28,8 @@ public class Player extends Rectangle{
     }
 
     public void render(Graphics g) {
-        g.setColor(Color.green);
-        g.fillRect(x, y, width, height);
+        // g.setColor(Color.green);
+        // g.fillRect(x, y, width, height);
+        g.drawImage(Spritesheet.player_front, x, y, 32, 32, null);
     }
 }

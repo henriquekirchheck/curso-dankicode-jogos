30076dc502104f9f9cd75c06feb69249048f95e2
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-14 16:58:04 -0300

    Documented all the tipes of variables

--- aula/src/aula/Main.java
+++ aula/src/aula/Main.java
@@ -2,9 +2,27 @@ package aula;
 
 public class Main {
     public static void main(String[] args) {
-        int idade = 14;
-        System.out.println("Minha idade é " + idade);
-        idade = 52;
-        System.out.println("Minha idade é " + idade);
+
+        // int idade = 14;
+        // System.out.println("Minha idade é " + idade);
+        // idade = 52;
+        // System.out.println("Minha idade é " + idade);
+
+
+
+        // Tipo de variaveis
+        int numero = 24; // Numeros
+        String frase = "Hello World"; // Palavras, frases e letras
+        boolean var = true; // Verdadeiro ou falso
+        char var2 = 'a'; // Uma unica letra
+        double var3 = 15.9; // Numeros flutuantes
+        long var4 = 294765456; // Numeros Longos
+
+        System.out.println(numero);
+        System.out.println(frase);
+        System.out.println(var);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
     }
 }

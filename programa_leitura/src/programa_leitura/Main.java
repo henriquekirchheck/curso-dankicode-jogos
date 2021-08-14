b144068dff5f2c4419338f43aaa99e211a36d3be
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-14 18:06:15 -0300

    A program that ask questions and show the results.

--- programa_leitura/src/programa_leitura/Main.java
+++ programa_leitura/src/programa_leitura/Main.java
@@ -5,8 +5,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String nome,cidade,estado,pais,peso,idade;
+
         System.out.println("Digite seu nome: ");
-        String nome = in.nextLine();
-        System.out.println(nome);
+        nome = in.nextLine();
+        System.out.println("Digite sua idade: ");
+        idade = in.nextLine();
+        System.out.println("Digite seu peso: ");
+        peso = in.nextLine();
+        System.out.println("Digite sua cidade: ");
+        cidade = in.nextLine();
+        System.out.println("Digite seu estado: ");
+        estado = in.nextLine();
+        System.out.println("Digite seu pais: ");
+        pais = in.nextLine();
+
+        System.out.println("-------------");
+        
+        // Gerar resultado do teste
+
+        System.out.println("O seu nome é: " + nome);
+        System.out.println("A sua idade é: " + idade);
+        System.out.println("O seu peso é: " + peso);
+        System.out.println("A sua cidade é: " + cidade);
+        System.out.println("O seu estado é: " + estado);
+        System.out.println("O seu pais é: " + pais);
     }
 }

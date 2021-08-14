adb5cf3915755dfecc8181d66704a095c4b4ef5e
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-14 18:23:05 -0300

    Commented everything out

--- programa_leitura/src/programa_leitura/Main.java
+++ programa_leitura/src/programa_leitura/Main.java
@@ -1,7 +1,7 @@
 package programa_leitura;
 
-import java.util.Random;
-import java.util.Scanner;
+// import java.util.Random;
+// import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {    
@@ -33,35 +33,35 @@ public class Main {
         // System.out.println("O seu estado é: " + estado);
         // System.out.println("O seu pais é: " + pais);
 
-        Scanner in = new Scanner(System.in);
-        Random rand = new Random();
+        // Scanner in = new Scanner(System.in);
+        // Random rand = new Random();
 
-        String nome, comando;
+        // String nome, comando;
 
-        System.out.println("Seja bem vindo ao jogo");
-        System.out.println("Insira seu nome: ");
-        nome = in.nextLine();
-        System.out.println("Sejá muito bem vindo(a) " + nome);
-        System.out.println("Você deseja avançar em qual direção? (w, a, s, d)");
+        // System.out.println("Seja bem vindo ao jogo");
+        // System.out.println("Insira seu nome: ");
+        // nome = in.nextLine();
+        // System.out.println("Sejá muito bem vindo(a) " + nome);
+        // System.out.println("Você deseja avançar em qual direção? (w, a, s, d)");
 
-        comando = in.nextLine();
+        // comando = in.nextLine();
 
-        if (comando.equals("w")) {
-            System.out.println("Você foi para frente!");
-            System.out.println("Um inimigo surgiu, o que deseja fazer? (a= atacar, f= fugir)");
-            comando = in.nextLine();
+        // if (comando.equals("w")) {
+        //     System.out.println("Você foi para frente!");
+        //     System.out.println("Um inimigo surgiu, o que deseja fazer? (a= atacar, f= fugir)");
+        //     comando = in.nextLine();
 
-            if (comando.equals("a")) {
-                if ((rand.nextInt(100) + 1) <= 75) {
-                    System.out.println("Você ganhou a batalha! Parabens!");
-                } else {
-                    System.out.println("Você perdeu a batalha");
-                }
-            } else if (comando.equals("f")) {
-                System.out.println("Você fugiu com sucesso! O jogo acabou");
-            }
-        } else if (comando.equals("s")) {
-            System.out.println("Você está entrando em outro mapa");
-        }
+        //     if (comando.equals("a")) {
+        //         if ((rand.nextInt(100) + 1) <= 75) {
+        //             System.out.println("Você ganhou a batalha! Parabens!");
+        //         } else {
+        //             System.out.println("Você perdeu a batalha");
+        //         }
+        //     } else if (comando.equals("f")) {
+        //         System.out.println("Você fugiu com sucesso! O jogo acabou");
+        //     }
+        // } else if (comando.equals("s")) {
+        //     System.out.println("Você está entrando em outro mapa");
+        // }
     }
 }

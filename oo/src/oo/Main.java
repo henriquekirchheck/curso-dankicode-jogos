2d266f78d20b3c1c3a3d90d99935ba6ca7635f2a
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-14 19:16:30 -0300

    Made oo (Orientado a Objeto)

---
+++ oo/src/oo/Main.java
@@ -0,0 +1,11 @@
+package oo;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+        Player player2 = new Player();
+        Enemy enemy = new Enemy();
+        player.initPlayer();
+        enemy.initEnemy();
+    }
+}
---
+++ oo/src/oo/Player.java
@@ -0,0 +1,7 @@
+package oo;
+
+public class Player {
+    public void initPlayer() {
+        System.out.println("Jogador iniciado");
+    }
+}

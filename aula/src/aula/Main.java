90ec5319e4cf45dfccc022f2b63be58ae0368839
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-14 17:10:00 -0300

    Put some arrays in

--- aula/src/aula/Main.java
+++ aula/src/aula/Main.java
@@ -8,21 +8,59 @@ public class Main {
         // idade = 52;
         // System.out.println("Minha idade é " + idade);
 
-
-
+        // ---------------------------------------------------------------------------------------- \\ 
+        
         // Tipo de variaveis
-        int numero = 24; // Numeros
-        String frase = "Hello World"; // Palavras, frases e letras
-        boolean var = true; // Verdadeiro ou falso
-        char var2 = 'a'; // Uma unica letra
-        double var3 = 15.9; // Numeros flutuantes
-        long var4 = 294765456; // Numeros Longos
 
-        System.out.println(numero);
-        System.out.println(frase);
-        System.out.println(var);
-        System.out.println(var2);
-        System.out.println(var3);
-        System.out.println(var4);
+        // int numero = 24; // Numeros
+        // String frase = "Hello World"; // Palavras, frases e letras
+        // boolean var = true; // Verdadeiro ou falso
+        // char var2 = 'a'; // Uma unica letra
+        // double var3 = 15.9; // Numeros flutuantes
+        // long var4 = 294765456; // Numeros Longos
+        
+        // System.out.println(numero);
+        // System.out.println(frase);
+        // System.out.println(var);
+        // System.out.println(var2);
+        // System.out.println(var3);
+        // System.out.println(var4);
+
+        // ---------------------------------------------------------------------------------------- \\ 
+        
+        // Constantes
+        
+        // final int maxLife = 100;
+        
+        // // ERRO
+        // // maxLife = 10;
+        
+        // System.out.println(maxLife);
+
+        // ---------------------------------------------------------------------------------------- \\ 
+        
+        // Arrays
+        
+        // // Array Normal
+        
+        // String[] nome = new String[3];
+        // nome[0] = "Henrique";
+        // nome[1] = "João";
+        // nome[2] = "Guilherme";
+        
+        // // Array Multidimencional (Pode ter quantas dimensões desejadas)
+        
+        // String[][] nome2 = new String[2][2];
+        
+        // nome2[0][0] = "Henrique";
+        // nome2[0][1] = "João";
+        // nome2[1][0] = "Guilherme";
+        // nome2[1][1] = "Ana";
+        
+        // ---------------------------------------------------------------------------------------- \\ 
+        
+        //
+
+        // ---------------------------------------------------------------------------------------- \\ 
     }
 }

fab0a86c75e05d403f576078295f18bb60656bda
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-14 16:49:37 -0300

    Added a variable

--- aula/src/aula/Main.java
+++ aula/src/aula/Main.java
@@ -2,6 +2,9 @@ package aula;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");        
+        int idade = 14;
+        System.out.println("Minha idade é " + idade);
+        idade = 52;
+        System.out.println("Minha idade é " + idade);
     }
 }

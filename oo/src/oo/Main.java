59427a4cc58971b2ef725e857e5521e9eec6cc22
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-14 19:28:55 -0300

    I made a big mess

--- oo/src/oo/Main.java
+++ oo/src/oo/Main.java
@@ -1,11 +1,31 @@
 package oo;
 
-public class Main {
+public class Main extends Teste_abstract{
     public static void main(String[] args) {
-        Player player = new Player();
-        Player player2 = new Player();
-        Enemy enemy = new Enemy();
-        player.initPlayer();
-        enemy.initEnemy();
+        // Player player = new Player();
+        // Player player2 = new Player();
+        // Enemy enemy = new Enemy();
+        // player.initPlayer();
+        // enemy.initEnemy();
+
+        // new Main().instanceMain2();
+    }
+
+    public void initGame() {
+
+    }
+
+    public void chamarAbstract() {
+        this.instanceMain2();
+    }
+
+    public void instanceMain2(){
+        new Main2().printHelloWorld();
+    }
+
+    private class Main2{
+        public void printHelloWorld() {
+            System.out.println("Hello World!");
+        }
     }
 }
---
+++ oo/src/oo/Teste_abstract.java
@@ -0,0 +1,7 @@
+package oo;
+
+public class Teste_abstract {
+    public void initGame() {
+        System.out.println("Iniciando jogo");
+    }
+}

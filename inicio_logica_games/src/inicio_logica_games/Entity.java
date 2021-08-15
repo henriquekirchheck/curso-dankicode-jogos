90f75be910937fcd1cdaed043206737f08ca4d5f
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-15 12:53:57 -0300

    Made a Entity class and a ArrayList

---
+++ inicio_logica_games/src/inicio_logica_games/Entity.java
@@ -0,0 +1,12 @@
+package inicio_logica_games;
+
+public class Entity {
+    private int vida;
+    private int x, y;
+
+    public Entity(int vida,int x,int y) {
+        this.vida = vida;
+        this.x = x;
+        this.y = y;
+    }
+}
--- inicio_logica_games/src/inicio_logica_games/Game.java
+++ inicio_logica_games/src/inicio_logica_games/Game.java
@@ -1,12 +1,28 @@
 package inicio_logica_games;
 
+import java.util.ArrayList;
+
 public class Game implements Runnable {
     private boolean isRunning;
     private Thread thread;
 
+    private ArrayList<Entity> entities = new ArrayList<Entity>();
+    
+    public Game() {
+        entities.add(new Entity(100, 25, 32));
+        entities.add(new Entity(100, 25, 33));
+
+        for(int i = 0; i < entities.size(); i++) {
+            Entity entity = entities.get(i);
+
+            
+        }
+    }
+
     public static void main(String[] args) {
         Game game = new Game();
         game.start();
+
     }
 
     public synchronized void start() {
@@ -17,12 +33,12 @@ public class Game implements Runnable {
 
     public void tick() {
         // Atualizar o jogo
-        System.out.println("Tick");
+        // System.out.println("Tick");
     }
     
     public void render() {
         // Renderiazar
-        System.out.println("Render");
+        // System.out.println("Render");
     }
 
     @Override

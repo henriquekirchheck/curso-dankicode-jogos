8138d7cf719bc42d9d688d9edef2e04f231bcede
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-08-15 11:32:42 -0300

    Made some variables

--- oo/src/oo/Main.java
+++ oo/src/oo/Main.java
@@ -1,36 +1,20 @@
 package oo;
 
 public class Main {
+    private String nome = "Henrique";
+    public static final int VIDA_MAXIMA = 100;
 
-    private static int exemplo(int[] a) {
-        int r = 0;
-        for (int i = 0; i < a.length; i++) {
-            r += a[i];
-        }
-        return r;
+    // Metodo Construtor
+    public Main(){
+        System.out.println("Classe criada por " + nome);
     }
 
-    public void print(String a) {
-        System.out.println(a);
+    public String getNome() {
+        return nome;
     }
 
     public static void main(String[] args) {
-        // Player2 player2 = new Player2();
-        // player2.nascer();
-        // Main main = new Main();
-        // main.exemplo();
-        // Main.exemplo();
-
-        int[] n1 = new int[5];
-
-        n1[0] = 10;
-        n1[1] = 10;
-        n1[2] = 10;
-        n1[3] = 10;
-        n1[4] = 10;
-
-        System.out.println(exemplo(n1));
-        new Main().print("Hello world!");
+        Main m = new Main();
+        System.out.println(m.getNome());
     }
-
 } 
